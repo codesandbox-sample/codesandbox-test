5308c3f523e5da0fac8a08c31493d769a8ef573f
css <148056189+codesandbox-sample@users.noreply.github.com>
2023-10-23 03:32:07 +0000

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -160,3 +160,27 @@ sayHello("you");
 // 配列の展開
 const arr1 = [1, 2];
 console.log(arr1);
+// ...arr1 配列の要素が順番に取得される
+console.log(...arr1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
+
+// 配列をまとめて取得
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(...arr3);
+
+// 配列のコピー、結合
+const arr4 = [10,20];
+const arr5 = [30,40];
+
+const arr6 = [...arr4];
+console.log(arr6);
+
+const arr7 = [...arr4,...arr5];
+console.logI(arr7);
+
